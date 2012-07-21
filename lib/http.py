7ae22076987b1e6cb46d19f8ac7f553c089dcacd
Jeff Knupp <jknupp@gmail.com>
2012-07-21 16:17:26 -0400

    clean up rusage a bit

--- lib/http.py
+++ lib/http.py
@@ -4,6 +4,23 @@ import os
 import os.path
 import resource
 
+RUSAGE = """0	{ru_utime}	time in user mode (float)
+{}	time in system mode (float)
+{}	maximum resident set size
+{}	shared memory size
+{}	unshared memory size
+{}	unshared stack size
+{}	page faults not requiring I/O
+{}	page faults requiring I/O
+{}	number of swap outs
+{}	block input operations
+{}	block output operations
+{}	messages sent
+{}	messages received
+{}	signals received
+{}	voluntary context switches
+{}	involuntary context switches"""
+
 
 class FileCache():
     """An in-memory cache of static files"""
@@ -43,42 +60,16 @@ class FileCache():
     def __str__(self):
         return '\n'.join(self.get_cache_stats())
 
-def print_usage_stats(rusage_struct):
 
-    return    '\
-0	{ru_utime}	time in user mode (float)\n \
-1	{ru_stime}	time in system mode (float)\n \
-2	{ru_maxrss}	maximum resident set size\n \
-3	{ru_ixrss}	shared memory size\n \
-4	{ru_idrss}	unshared memory size\n \
-5	{ru_isrss}	unshared stack size\n \
-6	{ru_minflt}	page faults not requiring I/O\n \
-7	{ru_majflt}	page faults requiring I/O\n \
-8	{ru_nswap}	number of swap outs\n \
-9	{ru_inblock}	block input operations\n \
-10	{ru_oublock}	block output operations\n \
-11	{ru_msgsnd}	messages sent\n \
-12	{ru_msgrcv}	messages received\n \
-13	{ru_nsignals}	signals received\n \
-14	{ru_nvcsw}	voluntary context switches\n \
-15	{ru_nivcsw}	involuntary context switches\n \
-'.format (
-	ru_utime   = rusage_struct.ru_utime,
-	ru_stime   = rusage_struct.ru_stime,
-	ru_maxrss  = rusage_struct.ru_maxrss,
-	ru_ixrss   = rusage_struct.ru_ixrss,
-	ru_idrss   = rusage_struct.ru_idrss,
-	ru_isrss   = rusage_struct.ru_isrss,
-	ru_minflt  = rusage_struct.ru_minflt,
-	ru_majflt  = rusage_struct.ru_majflt,
-	ru_nswap   = rusage_struct.ru_nswap,
-	ru_inblock = rusage_struct.ru_inblock,
-	ru_oublock = rusage_struct.ru_oublock,
-	ru_msgsnd  = rusage_struct.ru_msgsnd,
-	ru_msgrcv  = rusage_struct.ru_msgrcv,
-	ru_nsignals= rusage_struct.ru_nsignals,
-	ru_nvcsw   = rusage_struct.ru_nvcsw,
-	ru_nivcsw  = rusage_struct.ru_nivcsw  )
+def print_usage_stats(rusage_struct):
+    return  RUSAGE.format(rusage_struct.ru_utime, rusage_struct.ru_stime,
+    rusage_struct.ru_maxrss, rusage_struct.ru_ixrss, rusage_struct.ru_idrss,
+    rusage_struct.ru_isrss, rusage_struct.ru_minflt, rusage_struct.ru_majflt,
+    rusage_struct.ru_nswap, rusage_struct.ru_inblock, rusage_struct.ru_oublock,
+    rusage_struct.ru_msgsnd, rusage_struct.ru_msgrcv,
+    rusage_struct.ru_nsignals, rusage_struct.ru_nvcsw,
+    rusage_struct.ru_nivcsw)
+
 if __name__ == '__main__':
     cache = FileCache('/home/jeff/code/blug/generated/', 1)
     print (cache)
