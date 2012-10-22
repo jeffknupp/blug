8c15219486f9b2896f01f3e9ecfe19157ace0bd6
Jeff Knupp <jknupp@gmail.com>
2012-10-22 11:08:58 -0400

    fix javascript for github and twitter

--- static/javascripts/github.js
+++ static/javascripts/github.js
@@ -3,14 +3,15 @@ var github = (function(){
     var i = 0, fragment = '', t = $(target)[0];
 
     for(i = 0; i < repos.length; i++) {
-      fragment += '<li><a href="'+repos[i].url+'">'+repos[i].name+'</a><p>'+repos[i].description+'</p></li>';
+      fragment += '<li><a href="'+repos[i].html_url+'">'+repos[i].name+'</a><p>'+(repos[i].description||'')+'</p></li>';
     }
     t.innerHTML = fragment;
   }
   return {
     showRepos: function(options){
       $.ajax({
-          url: "https://api.github.com/users/"+options.user+"/repos
+          url: "https://api.github.com/users/"+options.user+"/repos?callback=?"
+        , type: 'jsonp'
         , error: function (err) { $(options.target + ' li.loading').addClass('error').text("Error loading feed"); }
         , success: function(data) {
           var repos = [];
