e7e027b90075f32258585b5fabd076ef1775eb9f
Jeff Knupp <jknupp@gmail.com>
2012-11-27 17:41:34 -0500

    add suggestions from Google PageSpeed tool

--- lib/blug_server.py
+++ lib/blug_server.py
@@ -3,6 +3,10 @@
 import os
 from http import server
 import resource
+import datetime
+import time
+import gzip
+import io
 
 RUSAGE = """0	{}	time in user mode (float)
 {}	time in system mode (float)
@@ -28,6 +32,9 @@ EOL2 = b'\n\n'
 class FileCacheRequestHandler(server.SimpleHTTPRequestHandler):
     """Request handler that serves cached versions of static files"""
 
+    expire_time = datetime.datetime.now() + datetime.timedelta(days=365)
+    timestamp = time.time()
+
     def do_GET(self):
         """Return the cached buffer created during initialization"""
         path = self.translate_path(self.path)
@@ -38,16 +45,24 @@ class FileCacheRequestHandler(server.SimpleHTTPRequestHandler):
         self.path = self.path.split('?', 1)[0]
         self.path = self.path.split('#', 1)[0]
         cType = self.guess_type(self.path)
-        file_buffer = self.server.file_cache.get_resource(self.path)
+        accept_encoding =  self.headers.get('Accept-Encoding', '')
+        if 'gzip' in accept_encoding:
+            file_buffer = self.server.file_cache.get_resource(self.path, zipped=True)
+        else:
+            file_buffer = self.server.file_cache.get_resource(self.path)
         if not file_buffer:
             self.send_error(404, "File not found")
             return None
 
         #TODO: Cache header along with file and send directly
         self.send_response(200)
-        self.send_header("Content-type", cType)
+        self.send_header("Content-type", cType + '; charset=UTF-8')
+        if cType != 'text/html':
+            self.send_header('Expires', self.expire_time)
+        if 'gzip' in accept_encoding:
+            self.send_header('Content-Encoding', 'gzip')
         self.send_header("Content-Length", len(file_buffer))
-        self.send_header("Last-Modified", self.date_time_string())
+        self.send_header("Last-Modified", self.date_time_string(self.timestamp))
         self.end_headers()
         self.wfile.write(file_buffer)
 
@@ -72,6 +87,7 @@ class FileCache():
     def __init__(self, base, debug=0):
         self.base = os.path.normpath(base)
         self.cache = dict()
+        self.gzip_cache = dict()
         self._debug = debug
         self.build_cache(self.base)
 
@@ -86,10 +102,14 @@ class FileCache():
                     self.build_cache(base_dir, name)
             else:
                 with open(name, 'rb') as input_file:
-                    self.cache[name[1:]] = bytes(input_file.read())
+                    data = bytes(input_file.read())
+                    self.cache[name[1:]] = data
+                    self.gzip_cache[name[1:]] = gzip.compress(data)
 
-    def get_resource(self, path):
+    def get_resource(self, path, zipped=False):
         """Returns the cached version of the file"""
+        if zipped and path in self.gzip_cache:
+            return memoryview(self.gzip_cache[path])
         if path in self.cache:
             return memoryview(self.cache[path])
         return None
