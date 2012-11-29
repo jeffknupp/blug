20b3df5a78c739f3a32dcaefc6050cea6d5f6142
Jeff Knupp <jknupp@gmail.com>
2012-11-29 11:29:50 -0500

    Bit of refactoring and PEP8 cleanup

--- blug.py
+++ blug.py
@@ -17,11 +17,10 @@ POST_SKELETON = """
 title: "{title}"
 date: {date}
 categories:
----
 """
 
 
-def generate_post_file_path(title, date):
+def generate_post_file_name(title, date):
     """Return the path a post should use based on its title and date"""
     post_file_date = datetime.datetime.strftime(date, '%Y/%m/%d/')
     title = ''.join(char for char in title.lower() if (
@@ -38,44 +37,48 @@ def get_all_posts(content_dir, blog_prefix, canonical_url, blog_root=None):
         if os.path.splitext(post_file_name)[1] != ".md":
             continue
 
+        post = dict()
         with open(os.path.join(content_dir, post_file_name), encoding='ascii') as post_file:
             post_file_buffer = post_file.read()
-
-        # Split the file into the YAML front matter and the post proper
-        (front_matter, _, post_body) = post_file_buffer.partition('\n---\n')
-        post = yaml.load(front_matter)
 
         # Generate HTML from Markdown, splitting between the teaser (the
         # content to display on the front page until <!--more--> is reached)
         # and the post proper
-        post['body'] = markdown.markdown(post_body, ['fenced_code',
-            'codehilite(force_linenos=True)', 'tables'])
-        (teaser, _, _) = post['body'].partition('<!--more-->')
-        post['teaser'] = teaser
-        post['categories'] = post['categories'].split()
+        md = markdown.Markdown(extensions=[
+            'fenced_code',
+            'codehilite(force_linenos=True)',
+            'tables',
+            'meta',
+        ])
+        generated_html = md.convert(post_file_buffer)
+        post['body'] = generated_html
+        post['title'] = md.Meta['title'][0]
+        (post['teaser'], _, _) = generated_html.partition('<!--more-->')
+        post['categories'] = md.Meta['categories'][0].split()
 
         # Construct datetime from the *incredibly useful* string YAML
         # provides
         post['date'] = datetime.datetime.strptime(
-                (post['date'].strip()), '%Y-%m-%d %H:%M')
+            (md.Meta['date'][0].strip()), '%Y-%m-%d %H:%M')
 
-        # In general we know the layout on disk much match the generated urls
+        # In general we know the layout on disk must match the generated urls
         # This doesn't hold in the case that there is an appendix to the
         # domain that the site resides on. For example, if my WidgetFactory
         # marketing department blog lived at
         # www.widgetfactory.com/marketing/blog/, we would generate the
         # files in the /blog sub-directory but the links would need to
         # include /marketing/blog
-        post['relative_path'] = generate_post_file_path(post['title'],
-                post['date']) + '/'
+        post['relative_path'] = generate_post_file_name(post['title'],
+                                                        post['date'])
         if blog_prefix:
             post['relative_path'] = os.path.join(
-                    blog_prefix, post['relative_path'])
+                blog_prefix, post['relative_path'])
 
-        post['relative_url'] = '/' + post['relative_path']
         if blog_root:
-            post['relative_url'] = os.path.join(
-                    '/', blog_root, post['relative_url'])
+            post['relative_url'] = os.path.join('/', blog_root,
+                                                post['relative_path'])
+        else:
+            post['relative_url'] = os.path.join('/', post['relative_path'])
 
         post['canonical_url'] = canonical_url + post['relative_url']
 
@@ -94,10 +97,10 @@ def create_path_to_file(path):
         os.makedirs(path)
 
 
-def generate_post(post, template_variables):
+def generate_post(post, template_variables, template_environment):
     """Generate a single post's HTML file"""
     output_path = os.path.join(template_variables['output_dir'],
-            post['relative_path'], 'index.html')
+                               post['relative_path'], 'index.html')
 
     if not post['body']:
         raise EnvironmentError('No content for post [{post}] found.'.format(
@@ -107,99 +110,112 @@ def generate_post(post, template_variables):
     post_vars = {'post': post}
 
     template_variables.update(post_vars)
-    template = template_variables['env'].get_template('post_index.html')
+    template = template_environment.get_template('post_index.html')
     create_path_to_file(output_path)
     with open(output_path, 'w') as output:
         output.write(template.render(template_variables))
 
 
-def generate_static_page(template_variables, output_dir, template_name,
-        filename='index.html'):
+def generate_static_page(template_variables, output_dir, template,
+                         filename='index.html'):
     """Generate a static page"""
-    template = template_variables['env'].get_template(template_name)
     create_path_to_file(output_dir)
     with open(os.path.join(output_dir, filename), 'w', encoding='ascii') as output_file:
         output_file.write(template.render(template_variables))
 
 
-def generate_static_files(site_config):
+def generate_static_files(site_config, posts, categories, template_environment):
     """Generate all 'static' files, files not based on markdown conversion"""
     # Not sure if this is Octopress silliness, but generate an index.html
     # at both the root level and the 'blog' level, so both www.foo.com and
     # www.foo.com/blog can serve the blog
-    site_config = copy(site_config)
-    site_config['next_page'] = 2
-    site_config['current_posts'] = site_config['all_posts'][:5]
-    generate_static_page(site_config,
-            site_config['output_dir'], 'list.html')
-    generate_static_page(site_config,
-            site_config['blog_dir'], 'list.html')
-    generate_static_page(site_config,
-            os.path.join(site_config['blog_dir'],
-                'archives'), 'archives.html')
-    generate_static_page(site_config,
-            os.path.join(site_config['output_dir'],
-                'about-me'), 'about.html')
-    generate_static_page(site_config, site_config['output_dir'],
-                'atom.xml', 'atom.xml')
-    for category, posts in site_config['categories'].items():
-        site_config['all_posts'] = posts
-        generate_static_page(site_config, os.path.join(site_config['blog_dir'],
-            'categories', category), 'archives.html')
-        generate_static_page(site_config, os.path.join(site_config['blog_dir'],
-            'categories', category), 'atom.xml', 'atom.xml')
+    list_template = template_environment.get_template('list.html')
+    archives_template = template_environment.get_template('archives.html')
+    atom_template = template_environment.get_template('atom.xml')
+    about_template = template_environment.get_template('about.html')
+
+    # Generate main 'index.html' and '/blog/index.html' pages,
+    # showing the five most recent posts
+    template_variables = copy(site_config)
+    template_variables['next_page'] = 2
+    template_variables['current_posts'] = posts[:5]
+    generate_static_page(template_variables,
+                         site_config['output_dir'], list_template)
+    generate_static_page(template_variables,
+                         site_config['blog_dir'], list_template)
+
+    # Generate blog archives page
+    template_variables['all_posts'] = posts
+    generate_static_page(template_variables,
+                         os.path.join(site_config['blog_dir'],
+                         'archives'), archives_template)
+
+    # Generate "About Me" page
+    generate_static_page(template_variables,
+                         os.path.join(site_config['blog_dir'],
+                         'about-me'), about_template)
+
+    # Generate atom.xml feed
+    template_variables['now'] = datetime.datetime.now().isoformat()
+    generate_static_page(template_variables, site_config['output_dir'],
+                         atom_template, 'atom.xml')
+
+    # Generate a category "archive" page listing the posts in each category
+    for category, posts in categories.items():
+        template_variables['all_posts'] = posts
+        generate_static_page(template_variables, os.path.join(site_config['blog_dir'],
+                             'categories', category), archives_template)
+        generate_static_page(template_variables, os.path.join(site_config['blog_dir'],
+                             'categories', category), atom_template, 'atom.xml')
 
 
-def generate_pagination_pages(site_config):
+def generate_pagination_pages(site_config, all_posts, template):
     """Generate the additional index.html files required for pagination"""
-    current_page = 1
-    site_config= copy(site_config)
-    all_posts = site_config['all_posts']
-    num_posts = len(site_config['all_posts'])
-    for page in [all_posts[index:index + 5] for index in range(5, len(all_posts), 5)]:
-        current_page += 1
+    template_variables = copy(site_config)
+    num_posts = len(all_posts)
+    for index, page in enumerate([all_posts[index:index + 5] for index in range(5, num_posts, 5)]):
+        # overcome the fact that enumerate is 0-indexed
+        current_page = index + 1
         # Since we're reusing the index.html template, make it think
         # these posts are the only ones
-        site_config['current_posts'] = page
-        site_config['next_page'] = current_page + 1
+        template_variables['current_posts'] = page
+        template_variables['next_page'] = current_page + 1
 
         # if we've reached the "last" page, don't present a link to older
         # content
         if current_page * 5 >= num_posts:
-            site_config['next_page'] = None
+            template_variables['next_page'] = None
 
         output_dir = os.path.join(site_config['blog_dir'],
-                'page', str(current_page))
-        generate_static_page(site_config, output_dir, 'list.html')
+                                  'page', str(current_page))
+        generate_static_page(template_variables, output_dir, template)
 
 
 def generate_all_files(site_config):
     """Generate all HTML files from the content directory using the site-wide
     configuration"""
     all_posts = get_all_posts(site_config['content_dir'],
-            site_config['blog_prefix'], site_config['url'],
-            site_config['blog_root'])
+                              site_config['blog_prefix'],
+                              site_config['url'],
+                              site_config['blog_root'])
     all_posts.sort(key=lambda i: i['date'], reverse=True)
     categories = collections.defaultdict(list)
     for post in all_posts:
         for category in post['categories']:
             categories[category].append(post)
 
-    site_config['now'] = datetime.datetime.now().isoformat()
-    site_config['all_posts'] = all_posts
-    site_config['categories'] = categories
-    site_config['env'] = jinja2.Environment(
-            loader=jinja2.FileSystemLoader(site_config['template_dir']))
+    template_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(
+                                              site_config['template_dir']))
 
-    generate_static_files(site_config)
-    generate_pagination_pages(site_config)
+    generate_static_files(site_config, all_posts, categories, template_environment)
+    generate_pagination_pages(site_config, all_posts, template_environment.get_template('list.html'))
 
     for index, post in enumerate(all_posts):
         try:
             post['post_previous'] = all_posts[index + 1]
         except IndexError:
             post['post_previous'] = all_posts[0]
-        generate_post(post, site_config)
+        generate_post(post, site_config, template_environment)
 
 
 def copy_static_content(output_dir, root_dir):
@@ -212,17 +228,17 @@ def copy_static_content(output_dir, root_dir):
 
 def create_post(title, content_dir):
     """Create an empty post with the YAML front matter generated"""
-    post_file_name = generate_post_file_path(title, datetime.datetime.now())
-    post_file_name = post_file_name.replace('/', '-') + '.md'
+    post_file_name = generate_post_file_name(title, datetime.datetime.now())
+    post_file_path = os.path.join(content_dir, post_file_name.replace('/', '-')) + '.md'
 
     post_date = datetime.datetime.strftime(
-            datetime.datetime.now(), '%Y-%m-%d %H:%M')
+        datetime.datetime.now(), '%Y-%m-%d %H:%M')
 
-    if os.path.exists(os.path.join(content_dir, post_file_name)):
-        raise EnvironmentError(
-                '[{post}] already exists.'.format(post=post_file_name))
+    if os.path.exists(os.path.join(content_dir, post_file_path)):
+        raise EnvironmentError('[{post}] already exists.'.format(
+            post=post_file_path))
 
-    with open(os.path.join(content_dir, post_file_name), 'w') as post_file:
+    with open(post_file_path, 'w') as post_file:
         post_file.write(POST_SKELETON.format(date=post_date, title=title))
 
 
@@ -231,17 +247,19 @@ def serve(*args, **kwargs):
     root = kwargs['root']
     os.chdir(root)
     handler = lib.blug_server.FileCacheRequestHandler
+
     def log_request(self, *args):
         """Do not log any requests while serving"""
         pass
 
-    httpd = lib.blug_server.BlugHttpServer(root, (kwargs['host'], int(kwargs['port'])),
-            handler)
+    httpd = lib.blug_server.BlugHttpServer(root, (kwargs['host'],
+                                           int(kwargs['port'])), handler)
 
     print("serving from {path} on port {port}".format(path=root,
-        port=kwargs['port']))
+                                                      port=kwargs['port']))
 
     httpd.serve_forever()
+
 
 def create_new_post(*args, **kwargs):
     """Creates a new blog post file with the correct path and YAML front matter filled in"""
@@ -261,10 +279,7 @@ def generate_site(*args, **kwargs):
     with open(config_file) as config_file_handle:
         site_config = yaml.load(config_file_handle.read())
 
-    site_config['blog_dir'] = site_config['output_dir']
-    if 'blog_prefix' in site_config:
-        site_config['blog_dir'] = os.path.join(site_config['output_dir'],
-            site_config['blog_prefix'])
+    site_config['blog_dir'] = os.path.join(site_config['output_dir'], site_config['blog_prefix'])
     print ('Generating...')
 
     copy_static_content(site_config['output_dir'], os.getcwd())
@@ -274,35 +289,36 @@ def generate_site(*args, **kwargs):
 def main():
     """Main execution of blug"""
     argument_parser = argparse.ArgumentParser(
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-            description='Generate a static HTML blog from \
-                    Markdown blog entries')
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description='Generate a static HTML blog from  Markdown blog entries')
     subparser = argument_parser.add_subparsers(help='help for sub-commands')
 
-    post_parser = subparser.add_parser('post', help='Create a blank blog post',
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    post_parser.add_argument('title',
-            help='Title for the blog post to be generated')
+    post_parser = subparser.add_parser(
+        'post', help='Create a blank blog post',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    post_parser.add_argument(
+        'title', help='Title for the blog post to be generated')
     post_parser.set_defaults(func=create_new_post)
 
-    generate_parser = subparser.add_parser('generate',
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-            help='Generate the complete static site using the posts\
-                    in the \'content\' directory')
+    generate_parser = subparser.add_parser(
+        'generate',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        help='Generate the complete static site using the posts\
+                in the \'content\' directory')
     generate_parser.set_defaults(func=generate_site)
 
-    serve_parser = subparser.add_parser('serve',
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-            help='Start an HTTP server that serves the files under the \
-                    <content-dir> directory')
+    serve_parser = subparser.add_parser(
+        'serve',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        help='Start an HTTP server that serves the files under the \
+                <content-dir> directory')
     serve_parser.add_argument('-p', '--port', default=8080,
-            help='Port for HTTP server to listen to')
-    serve_parser.add_argument('-s', '--host', action='store',
-            default='localhost',
-            help='Hostname for HTTP server to serve on')
-    serve_parser.add_argument('-r', '--root', action='store',
-            default=os.path.join(os.getcwd(), 'generated'),
-            help='Root path to serve files from')
+                              help='Port for HTTP server to listen to')
+    serve_parser.add_argument('-s', '--host', action='store', default='localhost',
+                              help='Hostname for HTTP server to serve on')
+    serve_parser.add_argument('-r', '--root', action='store', default=os.path.join(
+        os.getcwd(), 'generated'),
+        help='Root path to serve files from')
     serve_parser.set_defaults(func=serve)
 
     arguments = argument_parser.parse_args()
